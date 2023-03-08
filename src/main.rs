aa69839d8f302fd164cae7a0631b2d3b9dfdc215
GCast31 <asgardwyn@gmail.com>
2023-03-08 20:40:07 +0100

    new gc2d version

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@
 
 use std::collections::HashMap;
 
-use gc2d::{event::EventLoop, color::Color};
+use gc2d::{event::EventLoop, color::Color, image::Quad};
 use gc2d_games::tilemap::{TileMap, TypeTileMap, TileMapDetail, TileDescription};
 
 const FONT_MAIN: &str = "assets/fonts/PixelMaster.ttf";
 const FONT_MAIN_SIZE: u16 = 20;
 
-const MAP_TILE_HEIGHT: usize = 70;
-const MAP_TILE_WIDTH: usize = 70;
+const MAP_TILE_HEIGHT: usize = 16;
+const MAP_TILE_WIDTH: usize = 16;
 
 const WINDOW_HEIGHT: usize = MAP_TILE_HEIGHT * 10;
 const WINDOW_WIDTH: usize = MAP_TILE_WIDTH * 10;
@@ -51,16 +51,16 @@ impl Game {
     fn map_level1(&mut self) {
         if let Some(map) = &mut self.tilemap {
             map.set_map(Some(vec![
-                vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-                vec![2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-                vec![2, 2, 3, 2, 2, 2, 2, 2, 2, 2],
-                vec![2, 2, 2, 3, 2, 2, 2, 2, 2, 2],
-                vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-                vec![2, 2, 2, 2, 3, 2, 2, 2, 2, 2],
-                vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-                vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-                vec![2, 2, 2, 2, 2, 2, 2, 2, 1, 2],
-                vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+                vec![1, 3, 1, 3, 1, 3, 3, 3, 3, 1],
+                vec![1, 4, 4, 1, 1, 1, 4, 1, 4, 1],
+                vec![1, 5, 1, 5, 1, 5, 1, 5, 1, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             ]));
         }
     }
@@ -71,7 +71,9 @@ impl EventLoop for Game {
     fn load(&mut self, gc2d: &mut gc2d::gc2d::Gc2d, _audio_manager: &mut gc2d::audio::AudioManager) -> Result<(), gc2d::event::EventError> {
         // Informations of the WINDOW
         gc2d.window.set_title("Demo");
-        gc2d.window.set_size(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32);
+        gc2d.window.set_size(WINDOW_WIDTH as f32 * 3., WINDOW_HEIGHT as f32 * 3.);
+
+        gc2d.graphics.set_scale(3., 3.);
 
         // Add assets (fonts / images)
         gc2d.graphics.new_font(FONT_MAIN, FONT_MAIN_SIZE);
@@ -79,20 +81,39 @@ impl EventLoop for Game {
         gc2d.graphics.new_image("assets/images/grassCenter.png").unwrap();
         gc2d.graphics.new_image("assets/images/liquidLava.png").unwrap();
         gc2d.graphics.new_image("assets/images/liquidWater.png").unwrap();
+        gc2d.graphics.new_image("assets/images/tileset.png").unwrap();
 
         // Create the definition of each tile
         let tiles_definition = HashMap::from([
             (1, TileMapDetail { 
-                type_tilemap: TypeTileMap::FromSimpleFile("assets/images/grassCenter.png".to_string()),
+                type_tilemap: TypeTileMap::FromFile("assets/images/grassCenter.png".to_string(), None),
                 description: Some(MyTileDescription { name: String::from("Grass")}),
             }),
             (2, TileMapDetail {
-                type_tilemap: TypeTileMap::FromSimpleFile("assets/images/liquidLava.png".to_string()),
+                type_tilemap: TypeTileMap::FromFile("assets/images/liquidLava.png".to_string(), None),
                 description: Some(MyTileDescription { name: String::from("LiquidLava")}),
             }),
             (3, TileMapDetail {
-                type_tilemap: TypeTileMap::FromSimpleFile("assets/images/liquidWater.png".to_string()),
+                type_tilemap: TypeTileMap::FromFile("assets/images/liquidWater.png".to_string(), None),
                 description: Some(MyTileDescription { name: String::from("LiquidWater")}),
+            }),
+            (4, TileMapDetail {
+                type_tilemap: TypeTileMap::FromFile("assets/images/tileset.png".to_string(), Some(Quad {
+                    x: 0.,
+                    y: 0.,
+                    width: 16.,
+                    height: 16.,
+                })),
+                description: Some(MyTileDescription { name: String::from("Tileset 1-1")}),
+            }),
+            (5, TileMapDetail {
+                type_tilemap: TypeTileMap::FromFile("assets/images/tileset.png".to_string(), Some(Quad {
+                    x: 16.,
+                    y: 16.,
+                    width: 16.,
+                    height: 16.,
+                })),
+                description: Some(MyTileDescription { name: String::from("Tileset 2-2")}),
             }),
         ]);
 
@@ -116,11 +137,12 @@ impl EventLoop for Game {
             map.draw(gc2d);
 
             // Display informations of tile at position x, y of the mouse
-            if let Some(map_description) = map.get_tile_at_position(gc2d.mouse.x, gc2d.mouse.y) {
-                gc2d.graphics.print(
+            if let Some(map_description) = map.get_tile_at_position(gc2d.mouse.x, gc2d.mouse.y, &gc2d) {
+                gc2d.graphics.print_full(
                     format!("x: {}, y: {} : {}", gc2d.mouse.x, gc2d.mouse.y, map_description.name.clone()), 
-                    10., 10., 
+                    10., 10., 0., 0.30f32, 0.30f32, 0f32, 0f32,
                     Some(Color::BLACK), 
+
                     fonts
                 );
             }
--- src/tilemap.rs
+++ src/tilemap.rs
@@ -12,14 +12,13 @@
 
 use std::{collections::HashMap, hash::Hash};
 
-use gc2d::color::Color;
+use gc2d::{color::Color, image::Quad, gc2d::Gc2d};
 
 /*
     TYPE OF TILEMAP
  */
 pub enum TypeTileMap {
-    FromSimpleFile(String),
-    FromTileSet(String, u32, u32),
+    FromFile(String, Option<Quad>),
     Rectangle(Color),
 }
 
@@ -72,18 +71,20 @@ impl<T: Eq + Hash, U: TileDescription> TileMap<T, U> {
      * 
      * @brief: Get the at position x, y 
      */
-    pub fn get_tile_at_position(&self, x: f32, y: f32) -> Option<&U> {
+    pub fn get_tile_at_position(&self, x: f32, y: f32, gc2d: &Gc2d) -> Option<&U> {
 
         if let Some(map) = &self.map {
 
-            let closure = |pos: f32, size: f32| {
-                if self.tile_height != 0 {
-                    (pos / size).floor() as usize
+            let (sx, sy) = gc2d.graphics.get_scale();
+
+            let closure = |pos: f32, size: f32, scale: f32| {
+                if size != 0. && scale != 0. {
+                    (pos / (size * scale)).floor() as usize
                 } else { 0 }
             };
 
-            let line = closure(y, self.tile_height as f32);
-            let column = closure(x, self.tile_width as f32);
+            let line = closure(y, self.tile_height as f32, sy);
+            let column = closure(x, self.tile_width as f32, sx);
 
             if line < map.capacity() {
                 let tile_line = &map[line];
@@ -108,21 +109,25 @@ impl<T: Eq + Hash, U: TileDescription> TileMap<T, U> {
      */
     pub fn draw(&self, gc2d: &mut gc2d::gc2d::Gc2d) {
         if let Some(map) = &self.map {
+
             for (line, value_line) in map.iter().enumerate() {
                 for (column, value_column) in value_line.iter().enumerate() {
                     if let Some(tile_definition) = self.tiles_definition.get(&value_column) {
                         match &tile_definition.type_tilemap {
-                            TypeTileMap::FromSimpleFile(filename) => {
+                            TypeTileMap::FromFile(filename, quad) => {
+                                let draw_quad = if let Some(quad) = quad {
+                                    let my_quad = *quad;
+                                    Some(my_quad)
+                                } else {
+                                    None
+                                };
                                 gc2d.graphics.draw(
                                     filename.as_str(), 
-                                    None,
+                                    draw_quad,
                                     (self.tile_width * column) as f32 , 
                                     (self.tile_height * line) as f32 ,
                                     0f64,
                                 );
-                            },
-                            TypeTileMap::FromTileSet(_, _, _) => {
-                                todo!("GC2D Quad todo");
                             },
                             TypeTileMap::Rectangle(color) => {
                                 gc2d.graphics.rectangle(
